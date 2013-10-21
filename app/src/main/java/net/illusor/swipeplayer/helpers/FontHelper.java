7bbdd62fd4e4f8022df963f6efd69a88d3161f27
WINSE <win.accuracy@gmail.com>
2013-10-21 21:35:39 +0400

    ?????? ?????? ??????????

--- app/src/main/java/net/illusor/swipeplayer/helpers/FontHelper.java
+++ app/src/main/java/net/illusor/swipeplayer/helpers/FontHelper.java
@@ -8,4 +8,5 @@ public class FontHelper
     public static Typeface RobotoLight = Typeface.createFromAsset(SwipeApplication.getAppContext().getAssets(), "fonts/Roboto-Light.ttf");
     public static Typeface RobotoMedium = Typeface.createFromAsset(SwipeApplication.getAppContext().getAssets(), "fonts/Roboto-Medium.ttf");
     public static Typeface PTSans = Typeface.createFromAsset(SwipeApplication.getAppContext().getAssets(), "fonts/PTS55F.ttf");
+    public static Typeface PTSansNarrow = Typeface.createFromAsset(SwipeApplication.getAppContext().getAssets(), "fonts/PTN57F.ttf");
 }

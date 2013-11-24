4ab5258c8d55b650592561b3e614044c6c7943f1
WINSE <win.accuracy@gmail.com>
2013-11-24 17:14:47 +0400

    ??????????? ?????????? ??????????????? ?????? - ????? ?????? ??????????????? ? ????????? ?????

--- app/src/main/java/net/illusor/swipeplayer/services/SoundService.java
+++ app/src/main/java/net/illusor/swipeplayer/services/SoundService.java
@@ -1,5 +1,6 @@
 package net.illusor.swipeplayer.services;
 
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.Service;
 import android.content.BroadcastReceiver;
@@ -7,19 +8,17 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.media.AudioManager;
-import android.media.MediaPlayer;
 import android.os.Binder;
 import android.os.IBinder;
-import android.util.Log;
 import net.illusor.swipeplayer.domain.AudioFile;
 import net.illusor.swipeplayer.helpers.NotificationHelper;
 
-import java.io.IOException;
 import java.util.List;
 
 public class SoundService extends Service
 {
     public static final String ACTION_NEW_AUDIO = "net.illusor.swipeplayer.services.SoundService.NewAudio";
+    public static final String ACTION_QUIT = "net.illusor.swipeplayer.services.SoundService.Quit";
 
     public static final String INTENT_CODE_STOP = "net.illusor.swipeplayer.services.SoundService.STOP";
     public static final String INTENT_CODE_PAUSE = "net.illusor.swipeplayer.services.SoundService.PAUSE";
@@ -27,27 +26,20 @@ public class SoundService extends Service
 
     private static final int NOTIFICATION_CODE = 753951;
 
-    private List<AudioFile> playlist;
-    private AudioFile currentPlaying;
-
+    private final AudioPlayer audioPlayer = new AudioPlayer();
+    private final NoisyReceiver noisyReceiver = new NoisyReceiver();
+    private final NotificationHelper notificationHelper = new NotificationHelper(this);
+    private final AudioFocusChangeListener audioFocusChangeListener = new AudioFocusChangeListener();
     private AudioManager audioManager;
-    private MediaPlayer mediaPlayer;
-    private NoisyReceiver noisyReceiver;
-    private NotificationHelper notificationHelper;
-    private AudioFocusChangeListener audioFocusChangeListener;
-    private int mediaPlayerPauseTime;
+    private boolean serviceStarted;
 
     @Override
     public void onCreate()
     {
         super.onCreate();
         this.audioManager = (AudioManager)this.getSystemService(Context.AUDIO_SERVICE);
-        this.audioFocusChangeListener = new AudioFocusChangeListener();
-        this.noisyReceiver = new NoisyReceiver();
-        this.notificationHelper = new NotificationHelper(this);
     }
 
-    @Override
     public int onStartCommand(Intent intent, int flags, int startId)
     {
         if (intent != null && intent.getAction() != null)
@@ -81,151 +73,57 @@ public class SoundService extends Service
         return new SoundServiceBinder(this);
     }
 
-    private void play(AudioFile file)
+    void play(AudioFile audioFile)
     {
-        if (this.mediaPlayer != null)
-            this.mediaPlayer.release();
-
         int gain = this.audioManager.requestAudioFocus(this.audioFocusChangeListener, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN);
         if (gain != AudioManager.AUDIOFOCUS_REQUEST_GRANTED)
             return;
 
-        this.currentPlaying = file;
-
-        this.startService(new Intent(this, SoundService.class));
-        this.registerReceiver(this.noisyReceiver, new IntentFilter(AudioManager.ACTION_AUDIO_BECOMING_NOISY));
-        this.startForeground(NOTIFICATION_CODE, this.notificationHelper.getPlayingNotification(file));
-
-        this.broadcastNewAudioPlaying(file);
-
-        try
+        if (!this.serviceStarted)
         {
-            this.mediaPlayer = new MediaPlayer();
-            this.mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-            this.mediaPlayer.setDataSource(file.getAbsolutePath());
-            this.mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener()
-            {
-                @Override
-                public void onPrepared(MediaPlayer player)
-                {
-                    player.start();
-                }
-            });
-            this.mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener()
-            {
-                @Override
-                public void onCompletion(MediaPlayer player)
-                {
-                    playCompleted();
-                }
-            });
-            this.mediaPlayer.setOnErrorListener(new MediaPlayer.OnErrorListener()
-            {
-                @Override
-                public boolean onError(MediaPlayer mediaPlayer, int i, int i2)
-                {
-                    return false;
-                }
-            });
-            this.mediaPlayer.prepareAsync();
+            this.startService(new Intent(this, SoundService.class));
+            this.registerReceiver(this.noisyReceiver, new IntentFilter(AudioManager.ACTION_AUDIO_BECOMING_NOISY));
+            this.serviceStarted = true;
         }
-        catch (IOException e)
-        {
-            stop();
-            Log.e(this.getClass().getName(), e.getMessage());
-        }
+
+        this.audioPlayer.play(audioFile);
+        this.broadcastNewAudioPlaying(audioFile);
+
+        Notification notification = this.notificationHelper.getPlayingNotification(audioFile);
+        this.startForeground(NOTIFICATION_CODE, notification);
     }
 
-    private void pauseSilent()
+    void stop()
     {
-        if (this.mediaPlayer != null)
+        this.audioPlayer.stop();
+
+        if (this.serviceStarted)
         {
-            this.mediaPlayer.pause();
-            this.mediaPlayerPauseTime = this.mediaPlayer.getCurrentPosition();
+            this.unregisterReceiver(this.noisyReceiver);
+            this.audioManager.abandonAudioFocus(this.audioFocusChangeListener);
+            this.serviceStarted = false;
         }
+
+        this.broadcastQuit();
+        this.stopForeground(true);
+        this.stopSelf();
+
+        NotificationManager service = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+        service.notify(NOTIFICATION_CODE, this.notificationHelper.getStoppedNotification());
     }
 
     private void pause()
     {
-        if (this.mediaPlayer != null)
-        {
-            this.pauseSilent();
-            this.startForeground(NOTIFICATION_CODE, this.notificationHelper.getPausedNotification(this.currentPlaying));
-        }
+        this.audioPlayer.pause();
+        Notification notification = this.notificationHelper.getPausedNotification(this.audioPlayer.getAudioFile());
+        this.startForeground(NOTIFICATION_CODE, notification);
     }
 
     private void resume()
     {
-        if (this.mediaPlayer != null)
-        {
-            this.resume(this.mediaPlayerPauseTime);
-            this.startForeground(NOTIFICATION_CODE, this.notificationHelper.getPlayingNotification(this.currentPlaying));
-        }
-    }
-
-    private void resume(int milliseconds)
-    {
-        if (this.mediaPlayer != null)
-        {
-            this.mediaPlayer.seekTo(milliseconds);
-            this.mediaPlayer.start();
-        }
-    }
-
-    private void stop()
-    {
-        if (this.mediaPlayer != null)
-        {
-            this.unregisterReceiver(this.noisyReceiver);
-            this.mediaPlayer.release();
-            this.mediaPlayer = null;
-            this.audioManager.abandonAudioFocus(this.audioFocusChangeListener);
-
-            this.stopForeground(true);
-            this.stopSelf();
-
-            NotificationManager service = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
-            service.notify(NOTIFICATION_CODE, this.notificationHelper.getStoppedNotification());
-        }
-    }
-
-    private void setVolume(float volume)
-    {
-        this.mediaPlayer.setVolume(volume, volume);
-    }
-
-    private void playCompleted()
-    {
-        if (this.playlist == null || this.playlist.size() == 0)
-        {
-            this.stop();
-            return;
-        }
-        else
-        {
-            this.mediaPlayer.release();
-        }
-
-        int playlistSize = this.playlist.size();
-        int nextIndex = this.playlist.indexOf(this.currentPlaying) + 1;
-        if (nextIndex < 0 || nextIndex >= playlistSize)
-            nextIndex = 0;
-
-        int count = 0;
-        AudioFile newFile;
-        do
-        {
-            newFile = this.playlist.get(nextIndex);
-            count++;
-            nextIndex++;
-            if (nextIndex >= playlistSize)
-                nextIndex = 0;
-        } while (!newFile.exists() && count < playlistSize);
-
-        if (newFile.exists())
-            this.play(newFile);
-        else
-            this.stop();
+        this.audioPlayer.resume();
+        Notification notification = this.notificationHelper.getPlayingNotification(this.audioPlayer.getAudioFile());
+        this.startForeground(NOTIFICATION_CODE, notification);
     }
 
     private void broadcastNewAudioPlaying(AudioFile file)
@@ -235,9 +133,16 @@ public class SoundService extends Service
         this.sendBroadcast(intent);
     }
 
+    private void broadcastQuit()
+    {
+        Intent intent = new Intent(ACTION_QUIT);
+        this.sendBroadcast(intent);
+    }
+
     private class AudioFocusChangeListener implements AudioManager.OnAudioFocusChangeListener
     {
         private int lostFocusReason;
+        private AudioPlayerState audioPlayerState;
 
         @Override
         public void onAudioFocusChange(int i)
@@ -247,19 +152,21 @@ public class SoundService extends Service
                 case AudioManager.AUDIOFOCUS_LOSS:
                 {
                     this.lostFocusReason = i;
+                    this.audioPlayerState = audioPlayer.getState();
                     stop();
                     break;
                 }
                 case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT:
                 {
                     this.lostFocusReason = i;
+                    this.audioPlayerState = audioPlayer.getState();
                     pause();
                     break;
                 }
                 case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK:
                 {
                     this.lostFocusReason = i;
-                    setVolume(0.2f);
+                    audioPlayer.setVolume(0.2f);
                     break;
                 }
                 case AudioManager.AUDIOFOCUS_GAIN:
@@ -267,11 +174,16 @@ public class SoundService extends Service
                     switch (this.lostFocusReason)
                     {
                         case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT:
-                            resume();
+                        {
+                            if (this.audioPlayerState == AudioPlayerState.Playing)
+                                resume();
                             break;
+                        }
                         case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK:
-                            setVolume(1);
+                        {
+                            audioPlayer.setVolume(0.2f);
                             break;
+                        }
                     }
                     break;
                 }
@@ -313,53 +225,40 @@ public class SoundService extends Service
             this.soundService.resume();
         }
 
-        public void startSeek()
+        public void startRewind()
         {
-            this.soundService.pauseSilent();
+            this.soundService.audioPlayer.startRewind();
         }
 
-        public void endSeek(int milliseconds)
+        public void finishRewind(int milliseconds)
         {
-            this.soundService.resume(milliseconds);
+            this.soundService.audioPlayer.finishRewind(milliseconds);
+        }
+
+        public AudioFile getAudioFile()
+        {
+            return this.soundService.audioPlayer.getAudioFile();
+        }
+
+        public AudioPlayerState getState()
+        {
+            return this.soundService.audioPlayer.getState();
+        }
+
+        public int getPosition()
+        {
+            return this.soundService.audioPlayer.getPosition();
+        }
+
+        public int getDuration()
+        {
+            return this.soundService.audioPlayer.getDuration();
         }
 
         public void setPlaylist(List<AudioFile> playlist)
         {
-            this.soundService.playlist = playlist;
-        }
-
-        public AudioFile getCurrentFile()
-        {
-            return currentPlaying;
-        }
-
-        public int getProgress()
-        {
-            try
-            {
-                return mediaPlayer == null || !mediaPlayer.isPlaying()? 0 : mediaPlayer.getCurrentPosition();
-            }
-            catch (IllegalStateException e)
-            {
-                return 0;
-            }
-        }
-
-        public SoundServiceState getServiceState()
-        {
-            if (mediaPlayer == null)
-                return SoundServiceState.Stopped;
-            else if (mediaPlayer.isPlaying())
-                return SoundServiceState.Playing;
-            else
-                return SoundServiceState.Paused;
+            AudioPlayerOnCompleteBehavior behavior = new AudioPlayerNextTrackBehavior(playlist, this.soundService);
+            this.soundService.audioPlayer.setOnCompleteBehavior(behavior);
         }
    }
-
-    public enum SoundServiceState
-    {
-        Playing,
-        Paused,
-        Stopped
-    }
 }

731f2d88df2d868fac6361075e40e712ea725147
Nikita Kobzev <n.kobzev@netvoxlab.ru>
2014-03-03 10:17:10 +0400

    ????????? ?????????????? ????? ? ???????? ??????????? ??????? ????????? ??? ?????? folder-browser'? ? ???????? ??????? ????????

--- app/android/src/main/java/net/illusor/swipeplayer/widgets/DurationDisplayView.java
+++ app/android/src/main/java/net/illusor/swipeplayer/widgets/DurationDisplayView.java
@@ -15,9 +15,8 @@ limitations under the License.*/
 package net.illusor.swipeplayer.widgets;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.util.AttributeSet;
-import android.util.DisplayMetrics;
-import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -26,9 +25,12 @@ import net.illusor.swipeplayer.R;
 import net.illusor.swipeplayer.helpers.DimensionHelper;
 import net.illusor.swipeplayer.helpers.TimeFormatter;
 
+/**
+ * Displays timings when rewinding an audio track
+ */
 public class DurationDisplayView extends LinearLayout
 {
-    private final FormattedTextView textPassed, textLeft;
+    private final FormattedTextView textPassed, textLeft;//text "time passed" and "time left"
     private boolean cancelHide;
 
     public DurationDisplayView(Context context, AttributeSet attrs)
@@ -36,7 +38,6 @@ public class DurationDisplayView extends LinearLayout
         super(context, attrs);
 
         LayoutInflater.from(context).inflate(R.layout.durations_display, this);
-        this.setBackgroundResource(R.drawable.durations_display_bg);
         this.setOrientation(LinearLayout.VERTICAL);
         this.setGravity(Gravity.CENTER);
 
@@ -44,6 +45,8 @@ public class DurationDisplayView extends LinearLayout
         this.textLeft = (FormattedTextView)this.findViewById(R.id.id_duration_left);
         this.textPassed.setText("2:15");
         this.textLeft.setText("-1:05");
+
+        this.setColorsInverse(false);
     }
 
     @Override
@@ -81,23 +84,46 @@ public class DurationDisplayView extends LinearLayout
         }
     }
 
-    private void show()
+    /**
+     * Inverts the default layout colors
+     * @param inverse if colors should be inverted
+     */
+    public void setColorsInverse(boolean inverse)
     {
-        this.cancelHide = true;
-        super.setVisibility(View.VISIBLE);
+        Resources r = this.getContext().getResources();
+        this.textPassed.setColor(r.getColor(inverse ? R.color.color_durations_passed_inverse : R.color.color_durations_passed));
+        this.textLeft.setColor(r.getColor(inverse ? R.color.color_durations_left_inverse : R.color.color_durations_left));
+        this.setBackgroundResource(inverse ? R.drawable.durations_display_bg_inverse : R.drawable.durations_display_bg);
     }
 
-    private void hide()
-    {
-        if (!this.cancelHide)
-        super.setVisibility(View.GONE);
-    }
-
+    /**
+     * Sets the durations to display onto the view
+     * @param passed milliseconds passed since an audio track has began
+     * @param total total audio track length in milliseconds
+     */
     public void setDuration(int passed, int total)
     {
         final String timePassed = TimeFormatter.hhmmss(passed);
         final String timeLeft = TimeFormatter.hhmmss(total - passed);
         this.textPassed.setText(timePassed);
         this.textLeft.setText("-" + timeLeft);
+    }
+
+    /**
+     * Shows the view
+     */
+    private void show()
+    {
+        this.cancelHide = true;
+        super.setVisibility(View.VISIBLE);
+    }
+
+    /**
+     * Hides the view
+     */
+    private void hide()
+    {
+        if (!this.cancelHide)
+        super.setVisibility(View.GONE);
     }
 }
